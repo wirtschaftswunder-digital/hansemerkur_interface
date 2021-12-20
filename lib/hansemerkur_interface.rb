3cc601fe5b312492dec1d44567559d4563e70c63
Benny <btz@webprojektfabrik.de>
2021-12-20 10:57:03 +0100

    first commit

---
+++ lib/hansemerkur_interface.rb
@@ -0,0 +1,6 @@
+require "hansemerkur_interface/version"
+
+module HansemerkurInterface
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hansemerkur_interface/version.rb
@@ -0,0 +1,3 @@
+module HansemerkurInterface
+  VERSION = "0.1.0"
+end
